368a5e5dd16bf2185ae3c5897a8045489ba2758b
PavanBhaskarThota <pavanbhaskarthota05@gmail.com>
2023-09-29 19:24:48 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import MainRouter from './AllRoutes/MainRouter';
 import { Navbar } from './AllRoutes/Navbar';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Footer } from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainRouter/>
+      <ChakraProvider>
+        <Footer/>
+      </ChakraProvider>
     </div>
   );
 }
